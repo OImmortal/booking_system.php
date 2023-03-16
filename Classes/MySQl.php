b4b851f88ea9ef803af5599f4dee547381b0b84a
João Pedro <joaopedropeixotosaraiva@outlook.com>
2023-03-16 17:10:37 -0300

    Segundo commit - criação da conexão com o banco de dados

---
+++ Classes/MySQl.php
@@ -0,0 +1,25 @@
+<?php
+
+    class MySql {
+        private $conn;
+
+        public static function Connect() {
+            $file_json = file_get_contents("info.json");
+            $content_json = json_decode($file_json,false);
+        
+
+            if(self::$conn == null) {
+                try {
+                    self::$conn = new PDO('mysql:host=localhost;dbname='.$content_json->database, $content_json->user, $content_json->password);
+                    self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch (\Throwable $th) {
+                    echo "ERROR ".$th->getMessage();
+                }
+            }
+
+            return self::$conn;
+        }
+        
+    }
+
+?>
